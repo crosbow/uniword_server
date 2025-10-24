e36bcc8bb38f83a536d69cb01b0a98c90596eab1
jubayer <jubayerahmed.dev@gmail.com>
2025-10-24 15:16:50 +0600

    added cors

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
+import cors from "cors";
 import express from "express";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true }));
 
 // import router
 import definitionRouter from "./routes/definition.route.js";
-
 // route definition
 app.use("/api/v1/definition", definitionRouter);
 
